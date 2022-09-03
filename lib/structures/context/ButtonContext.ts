52b3289adfbd61afff056bda6a3fd6fc1ddb243c
Favian <favouriteteo@gmail.com>
2022-09-04 00:16:13 +0800

    Narrowed down button interaction type to ButtonInteraction

--- lib/structures/context/ButtonContext.ts
+++ lib/structures/context/ButtonContext.ts
@@ -1,4 +1,4 @@
-import { Client, Guild, MessageComponentInteraction } from "discord.js";
+import { ButtonInteraction, Client, Guild } from "discord.js";
 
 import { TritonClient } from "../..";
 import { Context } from "./Context";
@@ -6,7 +6,7 @@ import { Context } from "./Context";
 export class ButtonContext<C extends Client = TritonClient> extends Context<C> {
     public constructor(
         client: C,
-        public readonly interaction: MessageComponentInteraction,
+        public readonly interaction: ButtonInteraction,
         guild: Guild | null
     ) {
         super(client, guild);
