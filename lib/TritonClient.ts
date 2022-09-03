41cd47add89f7eb731c904340a32a03ef9b7a0a5
Favian <favouriteteo@gmail.com>
2022-09-03 22:47:06 +0800

    Added progress spinners for command registration and cleaned up code

--- lib/TritonClient.ts
+++ lib/TritonClient.ts
@@ -1,17 +1,24 @@
-import "dotenv/config";
-
+import assert from "assert";
+import chalk from "chalk";
 import { Client, ClientOptions, Collection, REST, Routes, Snowflake } from "discord.js";
+import dotenv from "dotenv";
 import fs from "fs/promises";
-import assert from "node:assert";
-import path from "node:path";
 import ora from "ora";
+import path from "path";
 import _ from "radash";
 
 import { SlashCommandContext } from "./structures/context";
 import { Command } from "./structures/interaction/command";
+import { CommandArgResolver } from "./structures/interaction/command/Command";
 import { DefaultLogger, Logger } from "./util/Logger";
 import { TritonError } from "./util/TritonError";
 import { Constructor } from "./util/types";
+
+dotenv.config({
+    path: path.join(process.cwd(), "./bot/.env"),
+});
+
+__dirname = path.join(process.cwd(), "./bot/src");
 
 export class TritonClient extends Client {
     public readonly options: TritonClientOptions;
@@ -23,30 +30,34 @@ export class TritonClient extends Client {
 
         assert(
             process.env.DISCORD_APP_ID,
-            "You must specify a Discord application ID in your .env file."
+            chalk.redBright`You must specify a Discord application ID in your .env file.`
         );
         assert(
-            process.env.DISCORD_APP_TOKEN,
-            "You must specify a Discord application token in your .env file."
-        );
-        assert(
-            process.env.NODE_ENV,
-            "You must set your 'NODE_ENV' variable in your .env file as 'development' or 'production'."
+            process.env.DISCORD_TOKEN,
+            chalk.redBright`You must specify a Discord application token in your .env file.`
         );
 
         this.options = options;
         this.commands = new Collection();
         this.util = {
-            logger: options.useDefaultLogger ? new DefaultLogger() : options.logger,
+            logger: this.options.useDefaultLogger
+                ? new DefaultLogger(undefined)
+                : this.options.logger(undefined),
         };
     }
 
     public async start() {
         await this.registerCommands();
+        await this.registerEvents();
+
+        const login = ora("Logging in...");
+        await this.login(process.env.DISCORD_TOKEN);
+        login.succeed(chalk.greenBright`${this.options.name} is ready!`);
     }
 
     private async import<T>(file: string) {
-        const Class = (await import(file)) as Constructor<T>;
+        // dynamic imports return an object
+        const Class = Object.values((await import(file)) as Record<string, Constructor<T>>)[0];
         return new Class();
     }
 
@@ -54,59 +65,78 @@ export class TritonClient extends Client {
         this.on("ready", () => {});
 
         this.on("interactionCreate", async interaction => {
-            if (interaction.isCommand()) {
-                const command = this.commands.get(interaction.commandName);
+            if (interaction.isChatInputCommand()) {
+                if (interaction.isCommand()) {
+                    const command = this.commands.get(interaction.commandName);
 
-                if (!command) {
-                    throw new TritonError("COMMAND_NOT_FOUND", interaction.commandName);
-                }
+                    if (!command) {
+                        throw new TritonError(e => e.CommandNotFound, interaction.commandName);
+                    }
 
-                const context = new SlashCommandContext(this, interaction);
+                    if (!command.isSlashCommand()) return;
 
-                for (const GuardFactory of command.options.guards ?? []) {
-                    const guard = new GuardFactory();
+                    await interaction.deferReply({
+                        ephemeral: command.options.ephemeral ?? true,
+                    });
 
-                    try {
-                        const passed = await guard.slashRun?.(context);
-                        if (!passed) {
-                            // yup extra stuff
-                            return;
-                        }
-                    } catch (e) {}
+                    const context = new SlashCommandContext(
+                        this,
+                        interaction,
+                        new CommandArgResolver(interaction),
+                        interaction.guild
+                    );
+
+                    for (const GuardFactory of command.options.guards ?? []) {
+                        const guard = new GuardFactory();
+
+                        try {
+                            if (guard.slashRun) {
+                                const passed = await guard.slashRun(context);
+                                if (!passed) {
+                                    await guard.onSlashFail!(context);
+                                    return;
+                                }
+                            }
+
+                            await interaction.editReply({
+                                content: guard.options.message,
+                            });
+                        } catch (e) {}
+                    }
+
+                    await command.slashRun?.(context);
                 }
             }
+        });
+
+        this.on("messageCreate", async message => {
+            if (message.author.bot) return;
         });
     }
 
     private async registerCommands() {
         const spinner = ora({
-            text: "Registering commands...",
+            text: chalk.cyanBright`Registering commands...`,
         }).start();
 
         const routeParsing = this.options.routeParsing;
         let folderPath: string | undefined;
 
         if (routeParsing.type === "default") {
-            folderPath = path.join(__dirname, `"./interactions/commands"`);
+            folderPath = path.join(__dirname, `./interactions/commands`);
         } else {
             folderPath = `${routeParsing.directories.baseDir}/${routeParsing.directories.commands}`;
 
             if (!folderPath) {
                 spinner.stopAndPersist({
-                    text: "No 'commands' directory specified. Skipping for now.",
+                    text: chalk.yellow`No 'commands' directory specified. Skipping for now.`,
                     prefixText: "❓",
                 });
                 return;
             }
         }
 
-        let fileNames: string[];
-
-        try {
-            fileNames = await fs.readdir(folderPath);
-        } catch (e) {
-            throw new TritonError("FOLDER_NOT_FOUND", folderPath);
-        }
+        const fileNames = await fs.readdir(folderPath!);
 
         const defaultFilter = (fileName: string) =>
             fileName.endsWith(".ts") || fileName.endsWith(".js");
@@ -120,30 +150,56 @@ export class TritonClient extends Client {
             const route = path.join(folderPath, file);
             const command = await this.import<Command>(route);
 
-            /* 
-                TODO: extra validation
-            */
+            for (const GuardFactory of command.options.guards ?? []) {
+                const guard = new GuardFactory();
+
+                if (command.isSlashCommand()) {
+                    assert(
+                        guard.slashRun,
+                        `${chalk.redBright("Guard ")}${chalk.cyanBright(
+                            `'${guard.options.name}'`
+                        )}${chalk.redBright(" must have a ")}${chalk.cyanBright(
+                            "'slashRun'"
+                        )}${chalk.redBright(" method for command ")}${chalk.cyanBright(
+                            `'${command.options.name}'`
+                        )}${chalk.redBright(".")}`
+                    );
+                }
+
+                if (command.isMessageCommand()) {
+                    assert(
+                        guard.messageRun,
+                        `${chalk.redBright("Guard ")}${chalk.cyanBright(
+                            `'${guard.options.name}'`
+                        )}${chalk.redBright(" must have a ")}${chalk.cyanBright(
+                            "'messageRun'"
+                        )}${chalk.redBright(" method for command ")}${chalk.cyanBright(
+                            `'${command.options.name}'`
+                        )}${chalk.redBright(".")}`
+                    );
+                }
+            }
 
             this.commands.set(command.options.name, command);
         }
 
         const rest = new REST({
             version: "10",
-        }).setToken(process.env.DISCORD_TOKEN);
+        }).setToken(process.env.DISCORD_TOKEN!);
 
         const devGuildIds = this.options.devGuildIds;
 
         if (devGuildIds && devGuildIds.length > 0) {
             assert(
                 process.env.NODE_ENV === "development",
-                "You've specified guild IDs for development, but you're not in development mode. Make sure that the 'NODE_ENV' variable in your .env file is set to 'development'."
+                chalk.redBright`You've specified guild IDs for development, but you're not in development mode. Make sure that the 'NODE_ENV' variable in your .env file is set to 'development'.`
             );
 
             for (const [index, guildId] of devGuildIds.entries()) {
-                const route = Routes.applicationGuildCommands(process.env.DISCORD_APP_ID, guildId);
+                const route = Routes.applicationGuildCommands(process.env.DISCORD_APP_ID!, guildId);
                 await rest.put(route, {
                     body: [
-                        this.commands
+                        ...this.commands
                             .filter(c => c.isSlashCommand())
                             .map(c => c.buildSlash().toJSON()),
                     ],
@@ -153,16 +209,20 @@ export class TritonClient extends Client {
                 })`;
             }
 
-            spinner.succeed(`Registered commands for ${devGuildIds.length} development guilds!`);
+            spinner.succeed(
+                chalk.green`Registered commands for ${devGuildIds.length} development ${
+                    devGuildIds.length !== 1 ? "guilds" : "guild"
+                }!`
+            );
             return;
         }
 
         assert(
             process.env.NODE_ENV === "production",
-            "You're not in production mode. Make sure that the 'NODE_ENV' variable in your .env file is set to 'production'."
+            chalk.redBright`You're not in production mode. Make sure that NODE_ENV in .env is set to 'production', OR add guild IDs in Triton's options.`
         );
 
-        const route = Routes.applicationCommands(process.env.DISCORD_APP_ID);
+        const route = Routes.applicationCommands(process.env.DISCORD_APP_ID!);
 
         await rest.put(route, {
             body: [
@@ -198,7 +258,7 @@ export type LoggerOptions =
       }
     | {
           useDefaultLogger: false;
-          logger: Logger;
+          logger: (channelId?: string) => Logger;
       };
 
 export type RouteParsingOptions = {
