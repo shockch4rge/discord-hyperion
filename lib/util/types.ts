889c860a968781c9c0320abd85a298dbb0fc4f22
Favian <favouriteteo@gmail.com>
2022-09-08 01:41:48 +0800

    Helper type for modifying interfaces/types in-place

--- lib/util/types.ts
+++ lib/util/types.ts
@@ -3,3 +3,6 @@ export type Constructor<T = new () => any> = new (
 ) => T;
 
 export type AnyConstructor<T = new () => any> = new (...args: any[]) => T;
+
+// https://stackoverflow.com/a/55032655/16144470
+export type Modify<T, R> = Omit<T, keyof R> & R;
