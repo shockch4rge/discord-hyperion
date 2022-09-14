a61fc613aaa8dee183a7958902b96e1b0200cde2
Favian <favouriteteo@gmail.com>
2022-09-14 22:28:29 +0800

    Include OwnerOnly guard as built-in feature

---
+++ lib/builtins/guards/OwnerOnly.ts
@@ -0,0 +1,26 @@
+import { HyperionClient } from "../../../lib";
+import { MessageCommandContext, SlashCommandContext } from "../../../lib/structures/context";
+import { Guard } from "../../../lib/structures/Guard";
+
+export class OwnerOnly extends Guard {
+    constructor() {
+        super({
+            name: "OwnerOnly",
+            message: "You must be the owner of the bot to use this command.",
+        });
+    }
+
+    public async slashRun(context: SlashCommandContext) {
+        return context.client.options.ownerIds.includes(context.interaction.user.id);
+    }
+
+    public async onSlashFail(context: SlashCommandContext<HyperionClient>) {
+        await context.reply({
+            content: this.options.message,
+        });
+    }
+
+    public async messageRun(context: MessageCommandContext): Promise<boolean> {
+        return context.client.options.ownerIds.includes(context.message.author.id);
+    }
+}
