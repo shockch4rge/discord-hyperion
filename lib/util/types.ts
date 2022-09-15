6501b9723fd2465caee5eeb6a79d4b72c2203e27
Favian <favouriteteo@gmail.com>
2022-09-15 14:06:50 +0800

    Add constructor type guard

--- lib/util/types.ts
+++ lib/util/types.ts
@@ -6,3 +6,7 @@ export type AnyConstructor<T = new () => any> = new (...args: any[]) => T;
 
 // https://stackoverflow.com/a/55032655/16144470
 export type Modify<T, R> = Omit<T, keyof R> & R;
+
+export const isConstructor = <T>(value: unknown): value is Constructor<T> => {
+    return typeof value === "function";
+};
