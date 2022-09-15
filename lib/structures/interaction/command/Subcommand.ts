d6af63e727a80048b04c2700d69e30da6b40d1bd
Favian <favouriteteo@gmail.com>
2022-09-15 13:55:15 +0800

    Renamed slashRun to run as it can only be activated via slash command

--- lib/structures/interaction/command/Subcommand.ts
+++ lib/structures/interaction/command/Subcommand.ts
@@ -4,7 +4,7 @@ import { Command, CommandOptions } from "./Command";
 export abstract class Subcommand<C extends Command = Command> {
     public constructor(public readonly command: C, public readonly options: SubcommandOptions) {}
 
-    public abstract slashRun(context: SlashCommandContext): Promise<void>;
+    public abstract run(context: SlashCommandContext): Promise<void>;
 }
 
 export type SubcommandOptions = Omit<CommandOptions, "channel" | "enableInDms" | "contextMenuType">;
