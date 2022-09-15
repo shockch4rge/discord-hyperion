bf11158809a0646ce781a36ab242d7041cc6c5f8
Favian <favouriteteo@gmail.com>
2022-09-15 14:04:53 +0800

    Include client as property in Event

--- lib/structures/Event.ts
+++ lib/structures/Event.ts
@@ -1,7 +1,12 @@
 import { ClientEvents } from "discord.js";
 
+import { HyperionClient } from "../HyperionClient";
+
 export abstract class Event<K extends keyof ClientEvents = keyof ClientEvents> {
-    public constructor(public readonly options: EventOptions<K>) {}
+    public constructor(
+        public readonly client: HyperionClient,
+        public readonly options: EventOptions<K>
+    ) {}
 
     public abstract run(...args: ClientEvents[K]): Promise<void>;
 }
