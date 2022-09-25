99d9c2bd4d94812ab42ac86b34b28f9754221798
Favian <favouriteteo@gmail.com>
2022-09-26 02:34:58 +0800

    Cleaned up file reading and clearer process for registering subcommands

--- lib/registries/CommandRegistry.ts
+++ lib/registries/CommandRegistry.ts
@@ -1,22 +1,30 @@
 import chalk from "chalk";
 import { ApplicationCommand, Collection, REST, Routes } from "discord.js";
 import assert from "node:assert/strict";
+import { Dirent } from "node:fs";
 import fs from "node:fs/promises";
 import ora from "ora";
 import path from "path";
 import * as _ from "radash";
 
+import { HyperionClient } from "../HyperionClient";
 import { Command, Subcommand } from "../structures/interaction/command";
 import { HyperionError } from "../util/HyperionError";
 import { isConstructor } from "../util/types";
 import { Registry } from "./Registry";
 
 export class CommandRegistry extends Registry<Command> {
+    public readonly rest: REST = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN!);
+
+    public constructor(client: HyperionClient, public readonly options: CommandRegistryOptions) {
+        super(client);
+    }
+
     private async importSubcommand(command: Command, path: string) {
         const [SubcommandClass] = Object.values(
-    		(await import(path)) as Record<string, new (command: Command) => Subcommand>
+            (await import(path)) as Record<string, new (command: Command) => Subcommand>
         );
-        
+
         const shortPath = path
             .match(/(?<=src).*/)?.[0]
             .replaceAll(/\\/g, "/")
@@ -28,7 +36,7 @@ export class CommandRegistry extends Registry<Command> {
         );
         assert(
             Subcommand.isPrototypeOf(SubcommandClass),
-            chalk.redBright`Object at ${path} must extend the Subcommand class!`
+            chalk.redBright`Class at ${shortPath} must extend the Subcommand class!`
         );
 
         return new SubcommandClass(command);
@@ -36,130 +44,72 @@ export class CommandRegistry extends Registry<Command> {
 
     public async register() {
         const devGuildIds = this.client.options.devGuildIds;
-        const shouldCleanLeftoverCommands = this.client.options.cleanLeftoverCommands ?? false;
-
-        const spinner = ora({
-            text: chalk.cyanBright`${
-                shouldCleanLeftoverCommands
-                    ? "Cleaning removed commands..."
-                    : "Registering commands..."
-            }`,
-        }).start();
-
-        const rest = new REST({
-            version: "10",
-        }).setToken(process.env.DISCORD_TOKEN!);
+        const shouldCleanLeftoverCommands = this.options.cleanLeftover ?? false;
 
         if (shouldCleanLeftoverCommands) {
-            for (const guildId of devGuildIds ?? []) {
-                const [err, commands] = await _.try(() =>
-                    rest.get(Routes.applicationGuildCommands(process.env.DISCORD_APP_ID!, guildId))
-                )();
-
-                if (err) {
-                    spinner.fail(chalk.redBright`Failed to get commands in guild ID [${guildId}].`);
-                    console.log(err);
-
-                    continue;
-                }
-
-                for (const command of commands as ApplicationCommand[]) {
-                    const [err] = await _.try(() =>
-                        rest.delete(
-                            Routes.applicationGuildCommand(
-                                process.env.DISCORD_APP_ID!,
-                                guildId,
-                                command.id
-                            )
-                        )
-                    )();
-
-                    if (err) {
-                        spinner.fail(
-                            chalk.redBright`Failed to delete command '${command.name}' in guild ID [${guildId}].`
-                        );
-                        throw new HyperionError(e => e.DeleteGuildCommandsFail, guildId);
-                    }
-                }
-            }
-
-            spinner.text = chalk.cyanBright`Registering commands...`;
+            await this.cleanGuildCommands(devGuildIds ?? []);
         }
 
-        const routeParsing = this.client.options.routeParsing;
-        let folderPath: string;
+        const spinner = ora({
+            text: chalk.cyanBright`Registering commands...`,
+        }).start();
 
-        if (routeParsing.type === "default") {
-            folderPath = path.join(this.importPath, `./interactions/commands`);
-        }
-        else {
-            const baseDir = routeParsing.directories.baseDir;
-            const commandDir = routeParsing.directories.commands;
+        const dirPath = path.join(this.importPath, `./interactions/commands`);
+        const commandDir = await fs.readdir(dirPath, { withFileTypes: true });
 
-            if (!baseDir || !commandDir) {
-                spinner.stopAndPersist({
-                    text: chalk.yellow`No 'commands' directory specified. Skipping for now.`,
-                    prefixText: "❓",
-                });
-                return;
-            }
-
-            folderPath = `${baseDir}/${commandDir}`;
-        }
-
-        const commandFolder = await fs.readdir(folderPath, { withFileTypes: true });
-
-        for (const file of commandFolder) {
+        for (const commandFile of commandDir) {
             // is subcommand
-            if (file.isDirectory()) {
-                const parentCommandFolder = await fs.readdir(path.join(folderPath, file.name), {
+            if (commandFile.isDirectory()) {
+                const parentCommandDir = await fs.readdir(path.join(dirPath, commandFile.name), {
                     withFileTypes: true,
                 });
 
+                const subcommandDirs = parentCommandDir.filter(f => f.isDirectory());
+                const parentCommandFile = parentCommandDir.find(f => f.isFile());
+
                 assert(
-                    parentCommandFolder.length === 2 &&
-                        parentCommandFolder.filter(f => !f.isDirectory()).length === 1 &&
-                        parentCommandFolder.filter(f => f.isDirectory()).length === 1,
-                    "A command with subcommands must have a single file and a folder."
+                    parentCommandDir.length === 2 &&
+                    subcommandDirs.length === 1 &&
+                    parentCommandFile &&
+                    this.isValidFile(parentCommandFile) &&
+                    subcommandDirs[0].name === "subcommands",
+                    chalk.redBright`A command with subcommands must be a folder that holds both a file with the same name, and a folder named 'subcommands'.`
                 );
 
                 const parentCommand = await this.import<Command>(
                     path.join(
-                        folderPath,
-                        file.name,
-                        parentCommandFolder.find(f => !f.isDirectory())!.name
+                        dirPath,
+                        commandFile.name,
+                        parentCommandFile.name
                     )
                 );
 
                 assert(
                     !parentCommand.isContextMenuCommand(),
-                    "A parentCommand with subcommands cannot be a context menu command."
+                    chalk.redBright`A parentCommand with subcommands cannot be a context menu command.`
                 );
                 assert(
                     !parentCommand.options.args || parentCommand.options.args.length === 0,
                     chalk.redBright`A command with subcommands cannot have arguments.`
                 );
 
-                const subcommandFolder = await fs.readdir(
+                const subcommandDir = await fs.readdir(
                     path.join(
-                        folderPath,
-                        file.name,
-                        parentCommandFolder.find(f => f.isDirectory())!.name
+                        dirPath,
+                        commandFile.name,
+                        parentCommandDir.find(f => f.isDirectory())!.name
                     ),
                     { withFileTypes: true }
                 );
 
                 // import all subcommands in its folder and register each one into a collection
                 const subcommands = (
-                    await Promise.all(
-                        subcommandFolder
-                            .filter(d => this.isValidFile(d))
-                            .map(d =>
-                                this.importSubcommand(
-                                    parentCommand,
-                                    path.join(folderPath, file.name, "subcommands", d.name)
-                                )
-                            )
+                    await Promise.all(subcommandDir
+                        .filter(d => this.isValidFile(d))
+                        .map(subcommandFile => this.importSubcommand(
+                            parentCommand,
+                            path.join(dirPath, commandFile.name, "subcommands", subcommandFile.name)
+                        ))
                     )
                 ).reduce(
                     (coll, sc) => coll.set(sc.options.name, sc),
@@ -171,14 +121,14 @@ export class CommandRegistry extends Registry<Command> {
                 continue;
             }
 
-            if (!this.isValidFile(file)) continue;
+            if (!this.isValidFile(commandFile)) continue;
 
-            const route = path.join(folderPath, file.name);
+            const route = path.join(dirPath, commandFile.name);
             const command = await this.import<Command>(route);
 
             assert(
                 !this.has(command.options.name),
-                `Command '${command.options.name}' already exists.`
+                chalk.redBright`Command '${command.options.name}' already exists.`
             );
 
             for (const GuardFactory of command.options.guards ?? []) {
@@ -233,63 +183,50 @@ export class CommandRegistry extends Registry<Command> {
         }
 
         const slashCommands = this.filter(command => command.isSlashCommand());
-        const contextMenuCommands = this.filter(command => command.isContextMenuCommand());
+        const cmCommands = this.filter(command => command.isContextMenuCommand());
 
         assert(
             slashCommands.size <= 100,
             chalk.redBright`You can only have 100 chat input commands per application.`
         );
         assert(
-            contextMenuCommands.size <= 10,
+            cmCommands.size <= 10,
             `${chalk.redBright
-                .bold`${contextMenuCommands.size}`}${chalk.redBright`/10 context menu commands registered.`}`
+                .bold`${cmCommands.size}`}${chalk.redBright`/10 context menu commands registered.`}`
         );
 
-        const userContextMenuCommands = contextMenuCommands.filter(
-            c => c.options.contextMenuType === "user" || c.options.contextMenuType === "client"
-        );
-        const messageContextMenuCommands = contextMenuCommands.filter(
-            c => c.options.contextMenuType === "message"
-        );
+        const [userCmCommands, messageCmCommands] = cmCommands.partition(c => c.options.contextMenuType === "user");
 
         assert(
-            userContextMenuCommands.size <= 5,
+            userCmCommands.size <= 5,
             `${chalk.redBright
-                .bold`${userContextMenuCommands.size}`}${chalk.redBright`/5 user context menu commands registered.`}`
+                .bold`${userCmCommands.size}`}${chalk.redBright`/5 user context menu commands registered.`}`
         );
         assert(
-            messageContextMenuCommands.size <= 5,
+            messageCmCommands.size <= 5,
             `${chalk.redBright
-                .bold`${messageContextMenuCommands.size}`}${chalk.redBright`/5 message context menu commands registered.`}`
+                .bold`${messageCmCommands.size}`}${chalk.redBright`/5 message context menu commands registered.`}`
         );
 
         if (devGuildIds && devGuildIds.length > 0) {
             assert(
                 process.env.NODE_ENV === "development",
-                chalk.redBright`You've specified guild IDs for development, but you're not in development mode. Make sure that the 'NODE_ENV' variable in your .env file is set to 'development'.`
+                chalk.redBright`You've specified guild IDs for development, but you're not in development mode. Make sure that the 'NODE_ENV' variable in your .env commandFile is set to 'development'.`
             );
 
             for (const [index, guildId] of devGuildIds.entries()) {
                 const route = Routes.applicationGuildCommands(process.env.DISCORD_APP_ID!, guildId);
-                await rest.put(route, {
+                await this.rest.put(route, {
                     body: [
                         ...slashCommands.map(command => command.buildSlash().toJSON()),
-                        ...contextMenuCommands.map(command => command.buildContextMenu().toJSON()),
+                        // ...cmCommands.map(command => command.buildContextMenu().toJSON()),
                     ],
                 });
-                spinner.text = `Registering commands in ${guildId}... (${index + 1}/${
-                    devGuildIds.length
-                })`;
+                spinner.text = `Registering commands in ${guildId}... (${index + 1}/${devGuildIds.length})`;
             }
 
             spinner.succeed(
-                chalk.green`Registered ${chalk.greenBright.bold(slashCommands.size)} slash ${
-                    slashCommands.size === 1 ? "command" : "commands"
-                } and ${chalk.greenBright.bold(contextMenuCommands.size)} context menu ${
-                    contextMenuCommands.size === 1 ? "command" : "commands"
-                } for ${chalk.greenBright.bold(devGuildIds.length)} development ${
-                    devGuildIds.length !== 1 ? "guilds" : "guild"
-                }!`
+                chalk.green`Registered ${chalk.greenBright.bold(slashCommands.size)} slash ${slashCommands.size === 1 ? "command" : "commands"} and ${chalk.greenBright.bold(cmCommands.size)} context menu ${cmCommands.size === 1 ? "command" : "commands"} for ${chalk.greenBright.bold(devGuildIds.length)} development ${devGuildIds.length !== 1 ? "guilds" : "guild"}!`
             );
             return;
         }
@@ -301,13 +238,55 @@ export class CommandRegistry extends Registry<Command> {
 
         const route = Routes.applicationCommands(process.env.DISCORD_APP_ID!);
 
-        await rest.put(route, {
+        await this.rest.put(route, {
             body: [
                 ...slashCommands.map(command => command.buildSlash().toJSON()),
-                ...contextMenuCommands.map(command => command.buildContextMenu().toJSON()),
+                // ...cmCommands.map(command => command.buildContextMenu().toJSON()),
             ],
         });
 
         spinner.succeed(`Registered global commands!`);
     }
+
+    private async cleanGuildCommands(guildIds: string[]) {
+        const spinner = ora({
+            text: chalk.cyanBright`Cleaning commands...`,
+        });
+
+        for (const guildId of guildIds) {
+            const [err, commands] = await _.try(() =>
+                this.rest.get(Routes.applicationGuildCommands(process.env.DISCORD_APP_ID!, guildId))
+            )();
+
+            if (err) {
+                spinner.fail(chalk.redBright`Failed to get commands in guild ID [${guildId}].`);
+                console.error(err);
+                continue;
+            }
+
+            for (const command of commands as ApplicationCommand[]) {
+                const [err] = await _.try(() =>
+                    this.rest.delete(
+                        Routes.applicationGuildCommand(
+                            process.env.DISCORD_APP_ID!,
+                            guildId,
+                            command.id
+                        )
+                    )
+                )();
+
+                if (err) {
+                    spinner.fail(
+                        chalk.redBright`Failed to delete command '${command.name}' in guild ID [${guildId}].`
+                    );
+                    throw new HyperionError(e => e.DeleteGuildCommandsFail, guildId);
+                }
+            }
+        }
+
+    }
 }
+
+export type CommandRegistryOptions = {
+    cleanLeftover?: boolean;
+};
