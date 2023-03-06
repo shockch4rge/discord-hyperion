f090745b8bc3895750f5f274b32dece5e5f0d031
Favian <favouriteteo@gmail.com>
2023-03-06 20:34:41 +0800

    refactor: replaced useTry with try-v2 package

--- lib/utils/Util.ts
+++ lib/utils/Util.ts
@@ -1,5 +1,6 @@
+import type { AnyConstructor } from "./types";
 
-export const isConstructor = (value: unknown): value is new (...args: any[]) => any => {
+export const isConstructor = (value: unknown): value is AnyConstructor => {
     return typeof value === "function" && value.toString().startsWith("class");
 };
 
@@ -9,27 +10,10 @@ export const buildNumberEmoji = (number: number) => {
     const numberString = number.toString();
     const isNegative = number < 0 && numberString.startsWith("-");
     const absoluteNumberString = isNegative ? numberString.slice(1) : numberString;
-    const digits = absoluteNumberString.split("");
 
-    return (isNegative ? "➖" : "") + digits.map(digit => numberEmojis[+digit])
+    return (isNegative ? "➖" : "") + absoluteNumberString
+        .split("")
+        .map(digit => numberEmojis[+digit])
         .join("");
 };
 
-export const useTry = async <T>(fn: (...args: any[]) => Promise<T>) => {
-    try {
-        return [null, await fn()] as [null, T];
-    }
-    catch (error) {
-        return [error] as [Error];
-    }
-};
-
-const test = async () => {
-    const [error, val] = await useTry(async () => {
-        return 3;
-    });
-
-    if (error) {
-        
-    }
-};
